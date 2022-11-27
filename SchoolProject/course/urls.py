fb5fee101a72433d01dcd726a1be012ccc8fbaa1
SaRu-N <saruniroula73@gmail.com>
2022-11-27 17:04:37 +0545

    add Student

--- SchoolProject/course/urls.py
+++ SchoolProject/course/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns =[
     path('tutorial/',views.django_tutorial),
-    path('info/',views.django_info),
+    path('info/',views.django_info,name='courseinfo'),
     path('project/',views.django_project),
 ]
