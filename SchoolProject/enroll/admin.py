a7476474c1aae4f2faa4b0c06a430f7d5ecca887
SaRu-N <saruniroula73@gmail.com>
2022-11-29 16:55:55 +0545

    Create and Register ModelAdmin Class using Decorator

--- SchoolProject/enroll/admin.py
+++ SchoolProject/enroll/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from enroll.models import Student
 # Register your models here.
-admin.site.register(Student)
+# using decorator
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display= ('id','stuid','stuname','stuemail','stupass')
+#no need of below code while using decorator
+#  admin.site.register(Student,StudentAdmin)
