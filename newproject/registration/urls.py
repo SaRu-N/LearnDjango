30566f089f96577ffe570ac822ab118cb8ab0c47
SaRu-N <saruniroula73@gmail.com>
2023-01-08 16:01:28 +0545

    Decorating in urls.py

--- newproject/registration/urls.py
+++ newproject/registration/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 from . import views
+from . views import ProfileView,AboutView,ContactView
+from django.contrib.auth.decorators import login_required,permission_required
+from django.contrib.admin.views.decorators import staff_member_required
 urlpatterns=[
     path('profile/',views.profile,name='profile'),
     path('about/',views.about,name='about'),
     path('contact/',views.contact,name='contact'),
+    path('profilecl/',views.ProfileView.as_view(),name='proflecl'),
+    path('aboutcl/',views.AboutView.as_view(),name='aboutcl'),
+    path('contactcl/',views.ContactView.as_view(),name='contactcl'),
+    path('profilecl/',login_required(ProfileView.as_view()),name='profilecl'),
+    path('aboutcl/',staff_member_required(AboutView.as_view()),name='aboutcl'),
+    path('contactcl/',permission_required("registration.can_add_user")(ContactView.as_view()),name='contactcl'),
 ]
--- newproject/registration/views.py
+++ newproject/registration/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.admin.views.decorators import staff_member_required
+from django.views.generic import TemplateView
 # Create your views here.
 @login_required
 def profile(request):
@@ -13,3 +14,23 @@ def about(request):
 def contact(request):
     return render(request,'registration/contact.html')
 
+
+#### Class Based Views
+class ProfileView(TemplateView):
+    template_name='registration/profile.html'
+    def get_context_data(self, **kwargs):
+        context= super().get_context_data(**kwargs)
+        context['view_type']='Class Based'
+        return context
+class AboutView(TemplateView):
+    template_name='registration/about.html'
+    def get_context_data(self, **kwargs):
+        context= super().get_context_data(**kwargs)
+        context['view_type']='Class Based'
+        return context
+class ContactView(TemplateView):
+    template_name='registration/contact.html'
+    def get_context_data(self, **kwargs):
+        context= super().get_context_data(**kwargs)
+        context['view_type']='Class Based'
+        return context
